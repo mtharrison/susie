2b7948e142fc6e97ecd5ad492de755c2dc9fb207
Matt Harrison <hi@matt-harrison.com>
2015-09-13 15:48:06 +0100

    Make linter hapi

--- example/index.js
+++ example/index.js
@@ -48,8 +48,11 @@ server.register([require('inert'), require('..')], function (err) {
 
             var c = 97;
             rs._read = function () {
+
                 rs.push(String.fromCharCode(c++));
-                if (c > 'z'.charCodeAt(0)) rs.push(null);
+                if (c > 'z'.charCodeAt(0)) {
+                    rs.push(null);
+                }
             };
 
             reply.event(rs);
